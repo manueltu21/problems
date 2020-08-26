d0387c1f7bd0e891bbf5b63584fd53e2e49b3f73
manueltu21 <70246924+manueltu21@users.noreply.github.com>
2020-08-26 07:31:11 -0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private String Texto;
+        private int periodo;
+        private String temporal;
+        int inicio, fin,repeticiones;
+        public String tx="";
+            public void validar() {
+                Console.Write("Ingrese la cadena de texto: ");
+                Texto = Console.ReadLine();
+                 inicio = Texto.IndexOf(Texto);
+                 fin = Texto.Length;
+            periodo = 3;
+            repeticiones = fin / periodo;
+            fin = fin - repeticiones+1;
+                temporal = Texto.Remove(inicio,fin);
+
+        }
+
+             public void imprimir() {
+                Console.WriteLine("Salida: " + temporal.Length);
+                
+             }
+
+        static void Main(string[] args)
+        {
+
+            Program programa1 = new Program();
+
+            programa1.validar();
+            programa1.imprimir();
+            Console.ReadKey();
+        }
+    }
+}
